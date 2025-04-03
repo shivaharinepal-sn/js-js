9d61181be4fb62756a1a48e4304732f060f26fc7
shivaharinepal-sn <shivaharinepal03@gmail.com>
2025-04-03 22:54:19 +0545

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // it treat all the JS code in new version//
+
+// alert (3 + 3) we are using node js not we browser//
+
+console.log(3+3);
+
+console.log("arjunnnnnn");
+
+let name= "Arjun Nepal";
+let age= 21
+let state;
+
+/*
+ String==> ""
+ number==> 2 tto the power 53 tyo vanda mathi vayo vani 
+ bigint use hunxa
+ boolean==> either true/ False
+ null==> standlone value
+ undefined==> value is not defined
+ symbol==> unique 
+
+
+ Object
+
+*/
+console.log(typeof null);
+console.log(typeof undefined);
+
---
+++ variable.js
@@ -0,0 +1,22 @@
+const accountId = 123455
+let accountEmail ="arjunnepal892@gmail.com"
+var accountPassword ="123456"
+accountCity = "Kathandu"
+let accountState;
+
+// accountId=2 not allowed
+
+accountEmail= "shivaharinepal03@gmail.com"
+accountPassword = "111111"
+accountCity = "Bhaktapur"
+
+
+console.log("accountId");
+
+/*
+prefer not to use var beause of the issue of block and functional scope
+*/
+
+
+console.table ([accountId, accountEmail, accountPassword,accountCity, accountState]);
+
